804718a42aa4cdca0c3d406bca423427f5e878b0
Ayush Ranjan <ayush.rajan@miracuves.com>
2024-11-11 22:47:22 +0530

    added menu, made the upcoming events page

--- src/sections/LatestPosts.tsx
+++ src/sections/LatestPosts.tsx
@@ -4,11 +4,20 @@ import { getPostColorFromCategory } from "../utils/postUtils";
 import { Tag } from "../components/Tag";
 import { CutCornerButton } from "../components/CutCornerButton";
 import { twMerge } from "tailwind-merge";
+import { useScroll, motion, useTransform } from "framer-motion";
+import { useRef } from "react";
 
 export const LatestPosts = (props: {
   latestPosts: CollectionEntry<"blog">[];
 }) => {
   const { latestPosts } = props;
+  const targetRef = useRef(null);
+
+  const { scrollYProgress } = useScroll({
+    target: targetRef,
+    offset: ["start end", "start center"],
+  });
+  const marginTop = useTransform(scrollYProgress, [0, 1], [0, 64]);
   return (
     <section className="py-60">
       <div className="container">
@@ -44,7 +53,13 @@ export const LatestPosts = (props: {
               )
             )}
           </div>
-          <div className="hidden md:flex flex-col gap-8 mt-16">
+          <motion.div
+            className="hidden md:flex flex-col gap-8 mt-16"
+            ref={targetRef}
+            style={{
+              marginTop,
+            }}
+          >
             {latestPosts.map(
               ({ data: { title, description, category } }, postIndex) => (
                 <Cards
@@ -65,7 +80,7 @@ export const LatestPosts = (props: {
                 </Cards>
               )
             )}
-          </div>
+          </motion.div>
         </div>
         <div className="flex justify-center mt-48 md:mt-32">
           <CutCornerButton>Read the Blog</CutCornerButton>
